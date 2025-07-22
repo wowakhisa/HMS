516f247344feaac50d8826442e72d2a2fd39836b
wowakhisa <minecraftlovers206@gmail.com>
2025-07-22 17:27:53 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'hmsunej_db');
+
+// Site configuration
+define('SITE_URL', 'http://localhost/hmsteknikunej-website');
+define('SITE_NAME', 'HMS UNEJ');
+
+// Error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
